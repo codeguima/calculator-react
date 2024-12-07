4e4ac71fe7546a6e5b656b16884b05f766cc5492
jhonnycodedev <111026602+codeguima@users.noreply.github.com>
2024-12-06 22:52:45 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Calculator from './main/Calculator';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+      <h1>Calculadora</h1>
+        <Calculator />
+  </div>,
 );
